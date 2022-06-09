15f51d7aafbfe0ba266393cf730b755c37e72301
nanashi1031 <k2ishida@icloud.com>
2022-06-09 16:31:13 +0900

    基盤

---
+++ Source/SceneGame.cpp
@@ -0,0 +1,82 @@
+#include "Graphics/Graphics.h"
+#include "SceneGame.h"
+
+// 初期化
+void SceneGame::Initialize()
+{
+}
+
+// 終了化
+void SceneGame::Finalize()
+{
+}
+
+// 更新処理
+void SceneGame::Update(float elapsedTime)
+{
+}
+
+// 描画処理
+void SceneGame::Render()
+{
+	Graphics& graphics = Graphics::Instance();
+	ID3D11DeviceContext* dc = graphics.GetDeviceContext();
+	ID3D11RenderTargetView* rtv = graphics.GetRenderTargetView();
+	ID3D11DepthStencilView* dsv = graphics.GetDepthStencilView();
+
+	// 画面クリア＆レンダーターゲット設定
+	FLOAT color[] = { 0.0f, 0.0f, 0.5f, 1.0f };	// RGBA(0.0～1.0)
+	dc->ClearRenderTargetView(rtv, color);
+	dc->ClearDepthStencilView(dsv, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+	dc->OMSetRenderTargets(1, &rtv, dsv);
+
+	// 描画処理
+	RenderContext rc;
+	rc.lightDirection = { 0.0f, -1.0f, 0.0f, 0.0f };	// ライト方向（下方向）
+
+	// ビュー行列
+	{
+		DirectX::XMFLOAT3 eye = { 0, 10, -10 };	// カメラの視点（位置）
+		DirectX::XMFLOAT3 focus = { 0, 0, 0 };	// カメラの注視点（ターゲット）
+		DirectX::XMFLOAT3 up = { 0, 1, 0 };		// カメラの上方向
+
+		DirectX::XMVECTOR Eye = DirectX::XMLoadFloat3(&eye);
+		DirectX::XMVECTOR Focus = DirectX::XMLoadFloat3(&focus);
+		DirectX::XMVECTOR Up = DirectX::XMLoadFloat3(&up);
+		DirectX::XMMATRIX View = DirectX::XMMatrixLookAtLH(Eye, Focus, Up);
+		DirectX::XMStoreFloat4x4(&rc.view, View);
+	}
+	// プロジェクション行列
+	{
+		float fovY = DirectX::XMConvertToRadians(45);	// 視野角
+		float aspectRatio = graphics.GetScreenWidth() / graphics.GetScreenHeight();	// 画面縦横比率
+		float nearZ = 0.1f;	// カメラが映し出すの最近距離
+		float farZ = 1000.0f;	// カメラが映し出すの最遠距離
+		DirectX::XMMATRIX Projection = DirectX::XMMatrixPerspectiveFovLH(fovY, aspectRatio, nearZ, farZ);
+		DirectX::XMStoreFloat4x4(&rc.projection, Projection);
+	}
+
+	// 3Dモデル描画
+	{
+
+	}
+
+	// 3Dデバッグ描画
+	{
+		// ラインレンダラ描画実行
+		graphics.GetLineRenderer()->Render(dc, rc.view, rc.projection);
+
+		// デバッグレンダラ描画実行
+		graphics.GetDebugRenderer()->Render(dc, rc.view, rc.projection);
+	}
+
+	// 2Dスプライト描画
+	{
+
+	}
+
+	// 2DデバッグGUI描画
+	{
+
+	}
+}
---
+++ Source/SceneGame.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// ゲームシーン
+class SceneGame
+{
+public:
+	SceneGame() {}
+	~SceneGame() {}
+
+	// 初期化
+	void Initialize();
+
+	// 終了化
+	void Finalize();
+
+	// 更新処理
+	void Update(float elapsedTime);
+
+	// 描画処理
+	void Render();
+};
