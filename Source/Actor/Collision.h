dd9b6160ddf6925cbf18df65cf1a0450b92139ca
nanashi1031 <k2ishida@icloud.com>
2022-07-12 16:46:53 +0900

    コリジョンクラスからHitResultを取り出す

--- Source/Actor/Collision.h
+++ Source/Actor/Collision.h
@@ -2,19 +2,18 @@
 
 #include "Graphics.h"
 
+// レイキャストのヒット結果
+struct HitResult
+{
+    DirectX::XMFLOAT3 collisionPosition = { 0, 0, 0 };
+    DirectX::XMFLOAT3 collisionNormal = { 0, 0, 0 };
+    float collisionDistance = 0.0f;
+    int collisionMaterialIndex = -1;
+};
+
 // コリジョン
 class Collision
 {
-public:
-    // レイキャストのヒット結果
-    struct HitResult
-    {
-        DirectX::XMFLOAT3 collisionPosition = { 0, 0, 0 };
-        DirectX::XMFLOAT3 collisionNormal = { 0, 0, 0 };
-        float collisionDistance = 0.0f;
-        int collisionMaterialIndex = -1;
-    };
-
 public:
     // 球と球の当たり判定
     static bool IntersectSphereVsSpherer(
