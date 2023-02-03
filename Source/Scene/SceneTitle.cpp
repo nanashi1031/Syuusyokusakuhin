42d53a642a62b0d291384fc4151be21e6736712f
nanashi1031 <k2ishida@icloud.com>
2023-02-03 10:17:32 +0900

    シーンタイトル時に、空を描画 残りはステージの描画とカメラの移動などをする

--- Source/Scene/SceneTitle.cpp
+++ Source/Scene/SceneTitle.cpp
@@ -1,31 +1,93 @@
-#include "Graphics/Graphics.h"
+#include "Graphics.h"
 #include "SceneTitle.h"
 #include "SceneGame.h"
 #include "SceneManager.h"
 #include "SceneLoading.h"
-#include "Input/Input.h"
+#include "Input.h"
+#include "StageManager.h"
+#include "StageMain.h"
+#include "LightManager.h"
+#include "Camera.h"
+#include "CameraController.h"
 
 //初期化
 void SceneTitle::Initialize()
 {
     //スプライト初期化
-    sprite = new Sprite("Data/Sprite/Title.png");
+    //title = new Sprite("Data/Sprite/Scene/Title.png");
+
+    StageManager& stageManager = StageManager::Instance();
+    StageMain* stageMain = new StageMain;
+    stageManager.Register(stageMain);
+
+	Graphics& graphics = Graphics::Instance();
+	Camera& camera = Camera::Instance();
+	camera.SetLookAt(
+		DirectX::XMFLOAT3(0, 10, -10),
+		DirectX::XMFLOAT3(0, 0, 0),
+		DirectX::XMFLOAT3(0, 1, 0)
+	);
+	camera.SetPerspectiveFov(
+		DirectX::XMConvertToRadians(45),
+		graphics.GetScreenWidth() / graphics.GetScreenHeight(),
+		0.1f,
+		1000.0f
+	);
+
+	{
+		mainDirectionalLight = new Light(LightType::Directional);
+		mainDirectionalLight->SetPosition({ 0.70f, 0.60f, 0.30f });
+		mainDirectionalLight->SetDirection({ 1, -1, -1 });
+		mainDirectionalLight->SetColor({ 0, 0, 0, 1 });
+		LightManager::Instane().Register(mainDirectionalLight);
+	}
+
+	// スカイボックス
+	{
+		Graphics& graphics = Graphics::Instance();
+		skyboxTexture = std::make_unique<Texture>(
+			"Data/Sprite/SkyTexture.jpg");
+		sprite = std::make_unique<Sprite>();
+		sprite->SetShaderResourceView(
+			skyboxTexture->GetShaderResourceView(),
+			skyboxTexture->GetWidth(), skyboxTexture->GetHeight());
+		sprite->Update(
+			0, 0,
+			graphics.GetScreenWidth(), graphics.GetScreenHeight(),
+			0, 0,
+			static_cast<float>(skyboxTexture->GetWidth()),
+			static_cast<float>(skyboxTexture->GetHeight()),
+			0,
+			1, 1, 1, 1);
+	}
 }
 
 //終了化
 void SceneTitle::Finalize()
 {
+    StageManager::Instance().Clear();
+	LightManager::Instane().Clear();
+
     //スプライト終了化
-    if (sprite != nullptr)
-    {
-        delete sprite;
-        sprite = nullptr;
-    }
+  //  if (title != nullptr)
+  //  {
+  //      delete title;
+		//title = nullptr;
+  //  }
 }
 
 //更新処理
 void SceneTitle::Update(float elapsedTime)
 {
+	DirectX::XMFLOAT3 target = { 0, 10, 0 };
+	CameraController::Instance().SetTarget(target);
+	CameraController::Instance().Update(elapsedTime);
+
+
+
+	Stage* stage = StageManager::Instance().GetStage(0);
+	StageManager::Instance().Update(elapsedTime);
+
     GamePad& gamePad = Input::Instance().GetGamePad();
 
     //何かボタンを押したらローディングシーンを挟んでゲームシーンへ切り替え
@@ -50,23 +112,129 @@ void SceneTitle::Render()
     ID3D11RenderTargetView* rtv = graphics.GetRenderTargetView();
     ID3D11DepthStencilView* dsv = graphics.GetDepthStencilView();
 
+	//Render3DScene();
+
     //画面クリア&レンダーターゲット設定
     FLOAT color[] = { 0.0f, 0.0f, 0.5f, 1.0f };     //RGBA(0.0~1.0)
     dc->ClearRenderTargetView(rtv, color);
     dc->ClearDepthStencilView(dsv, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
     dc->OMSetRenderTargets(1, &rtv, dsv);
 
+	RenderContext rc;
+	rc.deviceContext = dc;
+	{
+		// ライトの情報を詰め込む
+		LightManager::Instane().PushRenderContext(rc);
+
+		// カメラ
+		Camera& camera = Camera::Instance();
+		rc.viewPosition.x = camera.GetEye().x;
+		rc.viewPosition.y = camera.GetEye().y;
+		rc.viewPosition.z = camera.GetEye().z;
+		rc.viewPosition.w = 1;
+		rc.view = camera.GetView();
+		rc.projection = camera.GetProjection();
+
+		SpriteShader* shader = graphics.GetShader(SpriteShaderId::Skybox);
+		shader->Begin(rc);
+
+		shader->Draw(rc, sprite.get());
+
+		shader->End(rc);
+		ModelShader* modelShader = graphics.GetShader(ModelShaderId::Phong);
+		//modelShader->Begin(rc);
+
+		//StageManager::Instance().Render(rc, modelShader);
+
+		//modelShader->End(rc);
+	}
     //2Dスプライト描画
     {
         float screenWidth = static_cast<float>(graphics.GetScreenWidth());
         float screenHeight = static_cast<float>(graphics.GetScreenHeight());
-        float textureWidth = static_cast<float>(sprite->GetTextureWidth());
-        float textureHeight = static_cast<float>(sprite->GetTextureHeight());
+        //float textureWidth = static_cast<float>(title->GetTextureWidth());
+        //float textureHeight = static_cast<float>(title->GetTextureHeight());
         //タイトルスプライト描画
-        sprite->Render(dc,
+        /*title->Render(dc,
             0, 0, screenWidth, screenHeight,
             0, 0, textureWidth, textureHeight,
             0,
-            1, 1, 1, 1);
+            1, 1, 1, 1);*/
     }
+}
+
+// 3D空間の描画
+void SceneTitle::Render3DScene()
+{
+	Graphics& graphics = Graphics::Instance();
+	ID3D11DeviceContext* dc = graphics.GetDeviceContext();
+	ID3D11RenderTargetView* rtv = renderTarget->GetRenderTargetView().Get();
+	ID3D11DepthStencilView* dsv = graphics.GetDepthStencilView();
+
+	// 画面クリア＆レンダーターゲット設定
+	FLOAT color[] = { 0.0f, 0.0f, 0.5f, 1.0f };
+	dc->ClearRenderTargetView(rtv, color);
+	dc->ClearDepthStencilView(dsv, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+	dc->OMSetRenderTargets(1, &rtv, dsv);
+
+	// ビューポートの設定
+	D3D11_VIEWPORT	vp = {};
+	vp.Width = graphics.GetScreenWidth();
+	vp.Height = graphics.GetScreenHeight();
+	vp.MinDepth = 0.0f;
+	vp.MaxDepth = 1.0f;
+	dc->RSSetViewports(1, &vp);
+
+	// 描画処理
+	RenderContext rc;
+	rc.deviceContext = dc;
+
+	// ライトの情報を詰め込む
+	LightManager::Instane().PushRenderContext(rc);
+
+	//// シャドウマップの設定
+	//rc.shadowMapData.shadowMap = shadowmapDepthStencil->GetShaderResourceView().Get();
+	//rc.shadowMapData.lightViewProjection = lightViewProjection;
+	//rc.shadowMapData.shadowColor = shadowColor;
+	//rc.shadowMapData.shadowBias = shadowBias;
+
+	// カメラ
+	Camera& camera = Camera::Instance();
+	rc.viewPosition.x = camera.GetEye().x;
+	rc.viewPosition.y = camera.GetEye().y;
+	rc.viewPosition.z = camera.GetEye().z;
+	rc.viewPosition.w = 1;
+	rc.view = camera.GetView();
+	rc.projection = camera.GetProjection();
+
+	// スカイボックスの描画
+	{
+		SpriteShader* shader = graphics.GetShader(SpriteShaderId::Skybox);
+		shader->Begin(rc);
+
+		shader->Draw(rc, sprite.get());
+
+		shader->End(rc);
+	}
+
+	// 3Dモデル描画
+	{
+		ModelShader* modelShader = graphics.GetShader(ModelShaderId::Phong);
+		modelShader->Begin(rc);
+
+		StageManager::Instance().Render(rc, modelShader);
+
+		modelShader->End(rc);
+	}
+
+	// 3Dデバッグ描画
+	{
+#ifdef _DEBUG
+		// ラインレンダラ描画実行
+		graphics.GetLineRenderer()->Render(dc, rc.view, rc.projection);
+		// デバッグレンダラ描画実行
+		graphics.GetDebugRenderer()->Render(dc, rc.view, rc.projection);
+		LightManager::Instane().DrawDebugPrimitive();
+#endif
+	}
 }
--- Source/Scene/SceneTitle.h
+++ Source/Scene/SceneTitle.h
@@ -1,7 +1,12 @@
 #pragma once
 
-#include "Graphics/Sprite.h"
+#include "Sprite.h"
+#include "Texture.h"
 #include "Scene.h"
+#include "DepthStencil.h"
+#include "Light.h"
+#include "RenderTarget.h"
+#include "PostprocessingRenderer.h"
 
 //タイトルシーン
 class SceneTitle : public Scene
@@ -23,5 +28,30 @@ public:
     void Render() override;
 
 private:
-    Sprite* sprite = nullptr;
+    void Render3DScene();
+
+private:
+    std::unique_ptr<Sprite>	sprite;
+    std::unique_ptr<Sprite> title = nullptr;
+
+    // 平行光源データ
+    std::unique_ptr<Light> directional_light;
+    DirectX::XMFLOAT4 ambientLightColor;
+
+    // オフスクリーンレンダリング用描画ターゲット
+    std::unique_ptr<RenderTarget> renderTarget;
+
+    //	シャドウマップ用情報
+    Light* mainDirectionalLight = nullptr; // シャドウマップを生成する平行光源
+    std::unique_ptr<DepthStencil> shadowmapDepthStencil; //	シャドウマップ用深度ステンシルバッファ
+    float shadowDrawRect = 120.0f; // シャドウマップに描画する範囲
+    DirectX::XMFLOAT4X4	lightViewProjection; //	ライトビュープロジェクション行列
+    DirectX::XMFLOAT3 shadowColor = { 0.2f, 0.2f, 0.2f }; // 影の色
+    float shadowBias = 0.001f; // 深度比較用のオフセット値
+
+    // パノラマスカイボックス画像
+    std::unique_ptr<Texture> skyboxTexture;
+
+    // ポストプロセス
+    std::unique_ptr<PostprocessingRenderer>	postprocessingRenderer;
 };
