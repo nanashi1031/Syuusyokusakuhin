e01c52e1caaee50eff4f11f53d793c55ec5cc8b4
nanashi1031 <k2ishida@icloud.com>
2023-02-03 10:15:37 +0900

    エキス所持している場合、点光源を表示

--- Source/Actor/StateMachine/InsectDerived.cpp
+++ Source/Actor/StateMachine/InsectDerived.cpp
@@ -1,3 +1,4 @@
+#include <memory>
 #include "Input\Input.h"
 #include "InsectDerived.h"
 #include "Mathf.h"
@@ -6,6 +7,7 @@
 #include "PlayerManager.h"
 #include "Camera.h"
 #include "CameraController.h"
+#include "LightManager.h"
 
 void InsectState::IdleState::Enter()
 {
@@ -20,14 +22,15 @@ void InsectState::IdleState::Execute(float elapsedTime)
     DirectX::XMFLOAT3 length =
         Mathf::SubtractFloat3(owner->GetPosition(), player->GetPosition());
     length = Mathf::MakePlusFloat3(length);
-    if (length.x < 5.0f && length.z < 5.0f)
+    if (length.x < 1.0f && length.z < 1.0f)
     {
         owner->GetStateMachine()->ChangeState(Insect::State::Pursuit);
     }
 
     GamePad& gamePad = Input::Instance().GetGamePad();
     if (gamePad.GetButton() & GamePad::KEY_E)
-        owner->GetStateMachine()->ChangeState(Insect::State::Flying);
+        if (CameraController::Instance().GetLockOnFlag())
+            owner->GetStateMachine()->ChangeState(Insect::State::Flying);
 
     if (gamePad.GetButton() & GamePad::KEY_R)
         owner->GetStateMachine()->ChangeState(Insect::State::Return);
@@ -35,29 +38,47 @@ void InsectState::IdleState::Execute(float elapsedTime)
 
 void InsectState::IdleState::Exit()
 {
-
+    owner->SetClearVerocity();
 }
 
 void InsectState::PursuitState::Enter()
 {
+    if (owner->GetLightIndex() >= 0)
+    {
+        LightManager::Instane().RemoveIndex(owner->GetLightIndex());
+        owner->SetLightIndex(-1);
+        owner->SetExtractColor(-1);
+    }
+
+    PlayerManager& playerManager = PlayerManager::Instance();
+    Player* player = playerManager.GetPlayer(playerManager.GetplayerOneIndex());
+    if (owner->GetExtractColor() == ExtractColor::Heal)
+    {
+        player->SetHealth(player->GetHealth() + 30);
+    }
+    else if (owner->GetExtractColor() != ExtractColor::None)
+    {
+
+        player->SetExtractColor(owner->GetExtractColor());
+    }
 
 }
 
 void InsectState::PursuitState::Execute(float elapsedTime)
 {
-    Player* player  = PlayerManager::Instance().GetPlayer(PlayerManager::Instance().GetplayerOneIndex());
+    Player* player =
+        PlayerManager::Instance().GetPlayer(PlayerManager::Instance().GetplayerOneIndex());
     owner->SetPosition(player->GetPosition());
 
     GamePad& gamePad = Input::Instance().GetGamePad();
     if (gamePad.GetButton() & GamePad::KEY_E)
-        owner->GetStateMachine()->ChangeState(Insect::State::Flying);
-
-    owner->SetPosition({ owner->GetPosition().x, 3.0f, owner->GetPosition().z });
+        if (CameraController::Instance().GetLockOnFlag())
+            owner->GetStateMachine()->ChangeState(Insect::State::Flying);
 }
 
 void InsectState::PursuitState::Exit()
 {
-
+    owner->SetClearVerocity();
 }
 
 void InsectState::FlyingState::Enter()
@@ -74,19 +95,21 @@ void InsectState::FlyingState::Enter()
             enemy->GetParts()[targetIndex].name);
         DirectX::XMFLOAT3 lenght = Mathf::CalculateLength(
             enemy->GetNodePosition(node), owner->GetPosition());
-        owner->SetMoveVecX(lenght.x * 1000.0f);
-        owner->SetMoveVecZ(lenght.z * 1000.0f);
-        owner->SetVerocity(lenght);
+        owner->SetVerocity(Mathf::MultiplyFloat3Float(lenght, owner->GetMoveSpeed()));
     }
     else
     {
         Enemy* enemy = enemyManager.GetEnemy(0);
 
-        DirectX::XMFLOAT3 direction = Mathf::MultiplyFloat3Float(Camera::Instance().GetCameraDirection(), 1.0f);
+        DirectX::XMFLOAT3 direction =
+            Mathf::MultiplyFloat3Float(Camera::Instance().GetCameraDirection(), 1.0f);
+        direction.y = 0.0f;
         owner->SetTargetPosition(Mathf::MultiplyFloat3(enemy->GetPosition(), direction));
-
-        owner->SetMoveVecX(Camera::Instance().GetCameraDirection().x * 1000.0f);
-        owner->SetMoveVecZ(Camera::Instance().GetCameraDirection().z * 1000.0f);
+        DirectX::XMFLOAT3 destination =
+            Mathf::MultiplyFloat3Float(direction, owner->GetMoveSpeed());
+        DirectX::XMFLOAT3 lenght = Mathf::CalculateLength(
+            destination, owner->GetPosition());
+        owner->SetVerocity(Mathf::MultiplyFloat3Float(lenght, owner->GetMoveSpeed()));
     }
 
     timer = 0.0f;
@@ -94,8 +117,6 @@ void InsectState::FlyingState::Enter()
 
 void InsectState::FlyingState::Execute(float elapsedTime)
 {
-    //owner->MoveToTarget(elapsedTime, owner->GetMoveSpeed());
-
     // ‰ñ“](‰¼)
     //owner->SetAngle({ owner->GetAngle().x, owner->GetAngle().y, owner->GetAngle().z + 0.1f });
     //owner->Ratate({ 0.0f, 0.0f, 1.0f }, 1.0f);
@@ -111,7 +132,21 @@ void InsectState::FlyingState::Execute(float elapsedTime)
                 enemy, enemy->GetParts()[j].name, enemy->GetParts()[j].radius,
                 1.0f))
             {
-                owner->SetClearVerocity();
+                owner->SetExtractColor(enemy->GetParts()[j].extractColor);
+                DirectX::XMFLOAT4 color = Extract::ColorConversion(owner->GetExtractColor());
+
+                if (owner->GetLightIndex() >= 0)
+                {
+                    LightManager::Instane().RemoveIndex(owner->GetLightIndex());
+                }
+
+                Light* light = new Light(LightType::Point);
+                light->SetPosition(owner->GetPosition());
+                light->SetColor(DirectX::XMFLOAT4(color));
+                light->SetRange(2.0f);
+                LightManager::Instane().Register(light);
+                owner->SetLightIndex(LightManager::Instane().GetLightCount());
+
                 owner->SetPosition(owner->GetBeforePosition());
                 owner->GetStateMachine()->ChangeState(Insect::State::Idle);
             }
@@ -122,13 +157,14 @@ void InsectState::FlyingState::Execute(float elapsedTime)
     if (gamePad.GetButton() & GamePad::KEY_R)
         owner->GetStateMachine()->ChangeState(Insect::State::Return);
 
-    if (timer > 6.0f)
+    if (timer > 3.0f)
     {
         if (gamePad.GetButton() & GamePad::KEY_E)
-            owner->GetStateMachine()->ChangeState(Insect::State::Flying);
+            if (CameraController::Instance().GetLockOnFlag())
+                owner->GetStateMachine()->ChangeState(Insect::State::Flying);
     }
 
-    if (timer > 24.0f)
+    if (timer > 18.0f)
     {
         owner->GetStateMachine()->ChangeState(Insect::State::Idle);
     }
@@ -138,7 +174,7 @@ void InsectState::FlyingState::Execute(float elapsedTime)
 
 void InsectState::FlyingState::Exit()
 {
-
+    owner->SetClearVerocity();
 }
 
 void InsectState::ReturnState::Enter()
@@ -150,25 +186,31 @@ void InsectState::ReturnState::Execute(float elapsedTime)
 {
     PlayerManager& playerManager = PlayerManager::Instance();
     Player* player = playerManager.GetPlayer(playerManager.GetplayerOneIndex());
-    owner->SetTargetPosition({ player->GetPosition().x, 5.0f, player->GetPosition().z });
-    DirectX::XMFLOAT3 targetPos = { player->GetPosition().x, 5.0f, player->GetPosition().z };
+    DirectX::XMFLOAT3 targetPos = player->GetPosition();
+    targetPos.y += 2.0f;
     DirectX::XMFLOAT3 lenght = Mathf::CalculateLength(targetPos, owner->GetPosition());
-  /*  owner->SetMoveVecX(lenght.x * 1000.0f);
-    owner->SetMoveVecZ(lenght.z * 1000.0f);
-    owner->SetVerocity(lenght);*/
+    owner->SetVerocity(Mathf::MultiplyFloat3Float(lenght, owner->GetMoveSpeed()));
 
-    owner->MoveToTarget(elapsedTime, owner->GetMoveSpeed());
 
     // ‰ñ“](‰¼)
     //owner->SetAngle({ owner->GetAngle().x, owner->GetAngle().y, owner->GetAngle().z + 0.01f });
     //owner->Ratate({ 0.0f, 0.0f, 1.0f }, 1.0f);
 
+    DirectX::XMFLOAT3 length =
+        Mathf::SubtractFloat3(owner->GetPosition(), player->GetPosition());
+    length = Mathf::MakePlusFloat3(length);
+    if (length.x < 1.0f && length.z < 1.0f)
+    {
+        owner->GetStateMachine()->ChangeState(Insect::State::Pursuit);
+    }
+
     GamePad& gamePad = Input::Instance().GetGamePad();
     if (gamePad.GetButton() & GamePad::KEY_E)
-        owner->GetStateMachine()->ChangeState(Insect::State::Flying);
+        if (CameraController::Instance().GetLockOnFlag())
+            owner->GetStateMachine()->ChangeState(Insect::State::Flying);
 }
 
 void InsectState::ReturnState::Exit()
 {
-
+    owner->SetClearVerocity();
 }
