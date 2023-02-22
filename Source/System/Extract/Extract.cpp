40fc7bbb2bb47265b8d012b2d1219dec11507b41
nanashi1031 <k2ishida@icloud.com>
2023-02-22 09:40:45 +0900

    エキスクラス作成

--- Source/System/Extract/Extract.cpp
+++ Source/System/Extract/Extract.cpp
@@ -1,2 +1,184 @@
 #include "Extract.h"
 
+void Extract::Initialize()
+{
+    extractUI1 = std::make_unique<Sprite>("Data/Sprite/UI/ExtractUI1.png");
+    extractUI2 = std::make_unique<Sprite>("Data/Sprite/UI/ExtractUI2.png");
+    extractUI3 = std::make_unique<Sprite>("Data/Sprite/UI/ExtractUI3.png");
+
+    for (int i = 0; i < extractMax; i++)
+    {
+        extract.emplace_back(-0.01f);
+    }
+}
+
+void Extract::Update(float elapsedTime)
+{
+    for (int i = 0; i < extractMax; i++)
+    {
+        if (extract[i] > 0.0f)
+        {
+            extract[i] -= 1.0f * elapsedTime;
+        }
+    }
+}
+
+void Extract::Render2D(ID3D11DeviceContext* dc)
+{
+    float textureWidth = static_cast<float>(extractUI1->GetTextureWidth());
+    float textureHeight = static_cast<float>(extractUI1->GetTextureHeight());
+
+    DirectX::XMFLOAT4 color;
+    if (extract[0] > 0.00f)
+        color = Extract::Instance().ColorConversion(ExtractColor::Red);
+    else
+        color = Extract::Instance().ColorConversion(ExtractColor::None);
+
+    if (extract[0] < 5.0f)
+    {
+        if (colorFlag)
+        {
+            color.x += 0.02f;
+            if (color.x > 1.0f)
+                colorFlag = false;
+        }
+        else if (!colorFlag)
+        {
+            color.x -= 0.02f;
+            if (color.x < 0.0f)
+                colorFlag = true;
+        }
+    }
+
+    extractUI1->Render(
+        dc,
+        100, 0,
+        textureWidth / 2, textureHeight / 2,
+        0, 0,
+        textureWidth, textureHeight,
+        0,
+        color.x, color.y, color.z, color.w
+    );
+
+    textureWidth = static_cast<float>(extractUI2->GetTextureWidth());
+    textureHeight = static_cast<float>(extractUI3->GetTextureHeight());
+
+    if (extract[1] > 0.00f)
+        color = Extract::Instance().ColorConversion(ExtractColor::White);
+    else
+        color = Extract::Instance().ColorConversion(ExtractColor::None);
+
+    if (extract[1] < 5.0f)
+    {
+        if (colorFlag)
+        {
+            color.x += 0.02f;
+            color.y += 0.02f;
+            color.z += 0.02f;
+            if (color.x > 1.0f)
+                colorFlag = false;
+        }
+        else if (!colorFlag)
+        {
+            color.x -= 0.02f;
+            color.y -= 0.02f;
+            color.z -= 0.02f;
+            if (color.x <= 0.0f)
+                colorFlag = true;
+        }
+    }
+
+    extractUI2->Render(
+        dc,
+        170, 0,
+        textureWidth / 2, textureHeight / 2,
+        0, 0,
+        textureWidth, textureHeight,
+        0,
+        color.x, color.y, color.z, color.w
+    );
+
+    textureWidth = static_cast<float>(extractUI3->GetTextureWidth());
+    textureHeight = static_cast<float>(extractUI3->GetTextureHeight());
+
+    if (extract[2] > 0.00f)
+        color = Extract::Instance().ColorConversion(ExtractColor::Orange);
+    else
+        color = Extract::Instance().ColorConversion(ExtractColor::None);
+
+    if (extract[2] < 5.0f)
+    {
+        if (colorFlag)
+        {
+            color.x += 0.02f;
+            color.y += 0.01f;
+            if (color.x > 1.0f)
+                colorFlag = false;
+        }
+        else if (!colorFlag)
+        {
+            color.x -= 0.02f;
+            color.y -= 0.01f;
+            if (color.x <= 0.0f)
+                colorFlag = true;
+        }
+    }
+
+    if (extract[2] < 5.0f)
+    {
+        if (colorFlag)
+        {
+            color.x += 0.02f;
+            color.y += 0.02f;
+            color.z += 0.02f;
+            if (color.x > 1.0f)
+                colorFlag = false;
+        }
+        else if (!colorFlag)
+        {
+            color.x -= 0.02f;
+            color.y -= 0.02f;
+            color.z -= 0.02f;
+            if (color.x <= 0.0f)
+                colorFlag = true;
+        }
+    }
+
+    extractUI3->Render(
+        dc,
+        240, 0,
+        textureWidth / 2, textureHeight / 2,
+        0, 0,
+        textureWidth, textureHeight,
+        0,
+        color.x, color.y, color.z, color.w
+    );
+}
+
+void Extract::DrawDebugGUI()
+{
+    ImVec2 windowPosition = { 10, 10 };
+    ImGui::SetNextWindowPos(windowPosition, ImGuiCond_FirstUseEver);
+    ImVec2 windowSize = { 300, 300 };
+    ImGui::SetNextWindowSize(windowSize, ImGuiCond_FirstUseEver);
+    // ウィンドウの透明度
+    float alpha = 0.35f;
+    ImGui::SetNextWindowBgAlpha(alpha);
+
+    if (ImGui::Begin("Extract", nullptr))
+    {
+        //トランスフォーム
+        if (ImGui::CollapsingHeader("Transform"))
+        {
+            ImGui::DragFloat("positiona", &a, 0.1f);
+            ImGui::DragFloat("positionb", &b, 0.1f);
+            ImGui::DragFloat("positionc", &c, 0.1f);
+            ImGui::DragFloat("positiond", &d, 0.1f);
+
+            ImGui::SliderFloat("timerRed", &extract[0], 0.0f, extractTime);
+            ImGui::SliderFloat("timerWhite", &extract[1], 0.0f, extractTime);
+            ImGui::SliderFloat("timerOrange", &extract[2], 0.0f, extractTime);
+        }
+    }
+    ImGui::End();
+}
--- Source/System/Extract/Extract.h
+++ Source/System/Extract/Extract.h
@@ -1,20 +1,40 @@
 #pragma once
 #include <DirectXMath.h>
+#include"Graphics.h"
 
 enum class ExtractColor
 {
-    None,
     Red,
     White,
     Orange,
     Heal,
+    None,
 };
 
 class Extract
 {
+private:
+    struct ExtractInfo
+    {
+        int extractColor;
+        float timer;
+    };
+
 public:
+    static Extract& Instance()
+    {
+        static Extract extract;
+        return extract;
+    }
+
+    void Initialize();
+    void Finalize();
+    void Update(float elapsedTime);
+    void Render2D(ID3D11DeviceContext* dc);
+    void DrawDebugGUI();
+
     template<typename T>
-    static DirectX::XMFLOAT4 ColorConversion(T colorNo)
+    DirectX::XMFLOAT4 ColorConversion(T colorNo)
     {
         ExtractColor color = static_cast<ExtractColor>(colorNo);
         DirectX::XMFLOAT4 outColor;
@@ -27,8 +47,45 @@ public:
         else if (color == ExtractColor::Heal)
             outColor = { 0.0f, 1.0f, 0.0f, 1.0f };
         else
-            outColor = { 1.0f, 1.0f, 1.0f, 1.0f };
+            outColor = { 0.0f, 0.0f, 0.0f, 1.0f };
 
         return outColor;
     }
+
+    template <typename T>
+    float GetExtract(T index) { return extract[static_cast<int>(index)]; }
+
+    template<typename T>
+    void SetExtract(T color)
+    {
+        switch (static_cast<ExtractColor>(color))
+        {
+        case ExtractColor::Red:
+            extract[0] = extractTime;
+            break;
+        case ExtractColor::White:
+            extract[1] = extractTime;
+            break;
+        case ExtractColor::Orange:
+            extract[2] = extractTime;
+            break;
+        case ExtractColor::Heal:
+            break;
+        case ExtractColor::None:
+            break;
+        }
+    }
+
+private:
+    std::vector<float> extract;
+
+    const float extractTime = 30.0f;
+    const int extractMax = 3;
+    float colors = 0.0f;
+    float colorFlag = false;
+    float a, b, c, d = 1;
+
+    std::unique_ptr<Sprite> extractUI1 = nullptr;
+    std::unique_ptr<Sprite> extractUI2 = nullptr;
+    std::unique_ptr<Sprite> extractUI3 = nullptr;
 };
