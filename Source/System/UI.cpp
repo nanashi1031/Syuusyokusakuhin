b4820a174e1c2233050612292cc850cad4d63a3f
nanashi1031 <k2ishida@icloud.com>
2023-06-14 15:32:43 +0900

    UIをここで描画、実行(予定)

--- Source/System/UI.cpp
+++ Source/System/UI.cpp
@@ -1,2 +1,27 @@
 #include "UI.h"
+#include "Mathf.h"
 
+void UI::Initialize()
+{
+
+}
+
+void UI::Finalize()
+{
+
+}
+
+void UI::Update(float elapsedTime)
+{
+
+}
+
+void UI::Render2D(ID3D11DeviceContext* dc)
+{
+
+}
+
+void UI::DrawDebugGUI()
+{
+
+}
--- Source/System/UI.h
+++ Source/System/UI.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <DirectXMath.h>
+#include"Graphics.h"
+#include <vector>
 
 class UI
 {
@@ -16,4 +18,18 @@ public:
         DirectX::XMFLOAT4 color;
     };
 
+    struct DamegeUI
+    {
+        float damege;
+        float timer;
+        DirectX::XMFLOAT2 position;
+    };
+
+    void Initialize();
+    void Finalize();
+    void Update(float elapsedTime);
+    void Render2D(ID3D11DeviceContext* dc);
+    void DrawDebugGUI();
+
+    std::vector<float> parts;
 };
