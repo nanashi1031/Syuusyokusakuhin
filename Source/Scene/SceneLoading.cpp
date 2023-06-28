d39a1e4036ba43805e95df81231e4143536bdfab
nanashi1031 <k2ishida@icloud.com>
2023-06-28 14:02:06 +0900

    ロード画面の色変更

--- Source/Scene/SceneLoading.cpp
+++ Source/Scene/SceneLoading.cpp
@@ -51,7 +51,7 @@ void SceneLoading::Render()
     ID3D11DepthStencilView* dsv = graphics.GetDepthStencilView();
 
     //画面クリア&レンダーターゲット設定
-    FLOAT color[] = { 0.0f, 0.0f, 0.5f, 1.0f };     //RGBA(0.0f~1.0f)
+    FLOAT color[] = { 0.8f, 0.8f, 0.8f, 1.0f };     //RGBA(0.0f~1.0f)
     dc->ClearRenderTargetView(rtv, color);
     dc->ClearDepthStencilView(dsv, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
     dc->OMSetRenderTargets(1, &rtv, dsv);
