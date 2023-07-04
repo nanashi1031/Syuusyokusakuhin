cd949d708f35e0d2720e6ef3e1e41cabb7313eea
nanashi1031 <k2ishida@icloud.com>
2023-07-04 18:28:03 +0900

    Modelをユニークポインタに変更、それによる変更

--- Source/Graphics/Shader.h
+++ Source/Graphics/Shader.h
@@ -2,6 +2,7 @@
 
 #include <d3d11.h>
 #include <DirectXMath.h>
+#include <memory>
 #include "RenderContext.h"
 #include "Model.h"
 #include "Sprite.h"
@@ -16,7 +17,7 @@ public:
 	virtual void Begin(ID3D11DeviceContext* dc, const RenderContext& rc) = 0;
 
 	// ï`âÊ
-	virtual void Draw(ID3D11DeviceContext* dc, const Model* model) = 0;
+	virtual void Draw(ID3D11DeviceContext* dc, const std::shared_ptr<Model> model) = 0;
 
 	// ï`âÊèIóπ
 	virtual void End(ID3D11DeviceContext* context) = 0;
@@ -32,7 +33,7 @@ public:
 	virtual void Begin(const RenderContext& rc) = 0;
 
 	// ï`âÊ
-	virtual void Draw(const RenderContext& rc, const Model* model) = 0;
+	virtual void Draw(const RenderContext& rc, const std::shared_ptr<Model> model) = 0;
 
 	// ï`âÊèIóπ
 	virtual void End(const RenderContext& rc) = 0;
